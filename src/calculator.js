78be8c7cd2d72fd5e2dee77faef8d831e3d831cc
darkness8129 <yaroslaw460@gmail.com>
2020-07-07 17:13:22 +0300

    bug fixes, final version 1

--- src/calculator.js
+++ src/calculator.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         action = '',
         result = 0;
 
-    // after loading show 0 as a result
+    // after loading show '0' as a result
     $('#result').text(result);
 
     $('.action-btn').on('click', function () {
@@ -26,6 +26,8 @@ $(document).ready(function () {
                     // after receiving the result, it becomes the first number
                     firstNum = result;
 
+                    console.log(String(result).length)
+
                     secondNum = '';
                     $('#result').text(result);
 
@@ -41,7 +43,6 @@ $(document).ready(function () {
                     action = $(this).attr('data-action');
                     $('#action').text(`${firstNum}${action}`);
                 }
-
         }
     })
 
@@ -50,14 +51,64 @@ $(document).ready(function () {
 
         // if the action is entered, then write the first number, if not - the second
         if (!action) {
-            firstNum += number;
-            $('#action').append(number);
+            // length of the first number can not be > 10
+            if (firstNum.length > 10) {
+                number = '';
+            }
+
+            // there can be only one '.' in the first number and after first symbol
+            if (number === '.') {
+                if (!firstNum.includes('.') && firstNum) {
+                    firstNum += number;
+                    $('#action').append(number);
+                }
+                // only one '0' at the beginning of the number
+            } else if (number === '0') {
+                if (firstNum[0] !== '0') {
+                    firstNum += number;
+                    $('#action').append(number);
+                }
+                // 01 02 03... turns to 1 2 3
+            } else if (firstNum[0] === '0' && firstNum[1] !== '.') {
+                firstNum = '';
+                firstNum += number;
+                $('#action').text(number);
+            } else {
+                firstNum += number;
+                $('#action').append(number);
+            }
+
+
+
             // check so that you can't write a number after '='
         } else if (!$('#action').text().includes('=')) {
-            secondNum += number;
-            $('#action').append(number);
-        }
+            // length of the second number can not be > 10
+            if (secondNum.length > 10) {
+                number = '';
+            }
 
+            // there can be only one '.' in the second number and after first symbol
+            if (number === '.') {
+                if (!secondNum.includes('.') && secondNum) {
+                    secondNum += number;
+                    $('#action').append(number);
+                }
+                // only one '0' at the beginning of the number
+            } else if (number === '0') {
+                if (secondNum[0] !== '0') {
+                    secondNum += number;
+                    $('#action').append(number);
+                }
+                // 01 02 03... turns to 1 2 3
+            } else if (secondNum[0] === '0' && secondNum[1] !== '.') {
+                secondNum = '';
+                secondNum += number;
+                $('#action').text(`${firstNum}${action}${secondNum}`);
+            } else {
+                secondNum += number;
+                $('#action').append(number);
+            }
+        }
     })
 
     // func for calculating 
